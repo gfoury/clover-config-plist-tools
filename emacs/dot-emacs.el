f29bbf6235e546a702786627752b0d2ccb087822
gfoury <gfourymac@gmail.com>
2016-02-13 12:33:19 -0500

    for emacs: nXML mode validation for plists

---
+++ emacs/dot-emacs.el
@@ -0,0 +1,9 @@
+;; If you already have a custom-set-variables in your .emacs (and
+;; nobody will be reading this if they don't) then add the clause to
+;; the existing declaration, don't just copy custom-set-variables.
+
+(custom-set-variables
+  '(rng-schema-locating-files
+   (quote
+    ("schemas.xml" "/usr/local/Cellar/emacs/24.5/share/emacs/24.5/etc/schema/schemas.xml" "/Users/gfoury/.emacs.d/rng/schemas.xml"))))
+
